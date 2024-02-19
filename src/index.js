a68c6379bdf39baa594a683b96a3cf90599631d4
Finn Moffett <finn.moffett@gmail.com>
2024-02-19 12:32:14 +0000

    Add a form dialog and personalise to note format
    Added a form dialog inspired by a previous library project. Console
    logged the results to test that the inputs were being processes. Will
    make a function to add these to a notes object.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-console.log('test')
-
 import displayTask from "./displayTask";
 
-const taskBtn = document.getElementById('taskBtn');
+// Prevent submit page refresh defaults 
+const form = document.getElementById("form");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
 
+// Create task button
+const taskBtn = document.getElementById('taskBtn');
+// Dialog form element 
+const dialog = document.querySelector('dialog')
+
+// Form buttons
+const closeForm = document.querySelector(".closeForm")
+const confirmForm = document.querySelector(".submit")
+
+// Checking for the input elements
+const title = document.getElementById('title')
+const description = document.getElementById('description')
+const date = document.getElementById('date')
+const priority = document.getElementById('priority')
+
+// Open task form on pressing of task button
 taskBtn.addEventListener('click', () => {
   // addTask();
-  displayTask();
+  // displayTask();
+  dialog.showModal();
 })
+
+// close form on pressing of forms close or submit button
+closeForm.addEventListener('click', () => {
+  dialog.close();
+  console.log('test')
+});
+
+confirmForm.addEventListener('click', () => {
+  dialog.close();
+  console.log(title.value)
+  console.log(description.value)
+  console.log(date.value)
+  console.log(priority.value)
+});
+
 
 // press button => display form => apply values of form to add task function => apply values to display Task function
